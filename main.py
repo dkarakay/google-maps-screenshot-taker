62c9db41fda3dfc60cb2c56dcfbcf10eb3fb015e
Deniz Karakay <deniz@karakay.me>
2021-02-07 13:27:28 +0300

    Clean code

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-"""Take huge detailed screenshots of Google Maps."""
-
-from datetime import datetime
 import os
 import time
 import tkinter
@@ -8,6 +5,25 @@ import tkinter
 from PIL import Image
 import pyscreenshot
 from selenium import webdriver
+
+# Removing fields from Google Maps
+remove_from_view = [
+    "var element = document.getElementById(\"omnibox-container\");element.remove();",
+    "var element = document.getElementById(\"watermark\");element.remove();",
+    "var element = document.getElementById(\"vasquette\");element.remove();",
+    "var element = document.getElementsByClassName(\"app-viewcard-strip\");element[0].remove();",
+    "var element = document.getElementsByClassName(\"scene-footer-container\");element[0].remove();",
+
+]
+
+# Removing labels from Google Maps Satellite View
+remove_labels = [
+    "var element = document.getElementsByClassName(\"searchbox-hamburger-container\")[0];"
+    "element.getElementsByTagName(\"button\")[0].click();",
+    "var element = document.getElementsByClassName(\"widget-settings-earth-item\")[0];"
+    "element.getElementsByTagName(\"button\")[1].click();",
+
+]
 
 
 def create_map(lat_start: float, long_start: float, zoom: int,
@@ -19,10 +35,10 @@ def create_map(lat_start: float, long_start: float, zoom: int,
     """Create a big Google Map image from a grid of screenshots.
 
     ARGS:
-        lat_start: Top-left coodinate to start taking screenshots.
-        long_start: Top-left coodinate to start taking screenshots.
-        number_rows: Number of screenshots to take for map.
-        number_cols: Number of screenshots to take for map.
+        lat_start: Top-left coordinate to start taking screenshots.
+        long_start: Top-left coordinate to start taking screenshots.
+        number_rows: Number of rows to take screenshot.
+        number_cols: Number of columns to to create screenshot.
         scale: Percent to scale each image to reduce final resolution
             and filesize. Should be a float value between 0 and 1.
             Recommend to leave at 1 for production, and between 0.05
@@ -41,123 +57,102 @@ def create_map(lat_start: float, long_start: float, zoom: int,
         outfile: If provided, the program will save the final image to
             this filepath. Otherwise, it will be saved in the current
             working directory with name 'testing-<timestamp>.png'
+        offset_right: Right offset.
+        offset_top: Top offset.
+        offset_bottom: Bottom offset.
+        offset_left: Left offset.
     """
 
+    # Chromedriver should be in the current directory.
     PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
     DRIVER_BIN = os.path.join(PROJECT_ROOT, "chromedriver")
+
     driver = webdriver.Chrome(executable_path=DRIVER_BIN)
     driver.maximize_window()
-    # 2D grid of Images to be stitched together
-    images_maps = [[None for _ in range(number_cols)]
-                   for _ in range(number_rows)]
-    images_satellites = [[None for _ in range(number_cols)]
-                         for _ in range(number_rows)]
 
     # Calculate amount to shift lat/long each screenshot
     screen_width, screen_height = get_screen_resolution()
 
-    lat_shift = calc_latitude_shift(screen_height,
-                                    (offset_top + offset_bottom))
-    print(f"Lat shift: {lat_shift}")
+    # Shifting values for lat and long
+    lat_shift = calc_latitude_shift(screen_height, (offset_top + offset_bottom), zoom)
+    long_shift = calc_longitude_shift(screen_width, (offset_left + offset_right), zoom)
 
-    lat_shift = calc_latitude_shift(screen_height, (offset_top + offset_bottom)) * 1 / 1.7 ** (zoom - 18)
-    print(f"Lat shift: {lat_shift}")
-
-    long_shift = calc_longitude_shift(screen_width,
-                                      (offset_left + offset_right)) * 1 / 1.7 ** (zoom - 18)
-    print(f"Long shift: {long_shift}")
-
+    # Counting for map and image view
     c_map = count
     c_image = count
-    for i in range(0,1):
+
+    """
+    i = 0 -> Map View
+    i = 1 -> Satellite View
+    """
+    for i in range(2):
         for row in range(number_rows):
             for col in range(number_cols):
+
                 latitude = lat_start + (lat_shift * row)
                 longitude = long_start + (long_shift * col)
-                # Show the map using the Firefox driver
-                url = ('https://www.google.com/maps/@{lat},{long},{z}z').format(lat=latitude, long=longitude, z=zoom)
-                if c_map >= 225 and c_map < 226:
-                    if i == 1:
-                        url = (
-                            'https://www.google.com/maps/@{lat},{long},{z}z/data=!3m1!1e3'
-                        ).format(lat=latitude, long=longitude, z=zoom)
 
-                    driver.get(url)
-                    time.sleep(5)
+                # Map URL
+                url = 'https://www.google.com/maps/@{lat},{long},{z}z'.format(lat=latitude, long=longitude, z=zoom)
 
-                    if i == 1:
-                        js_string = "var element = document.getElementsByClassName(\"searchbox-hamburger-container\")[0];" \
-                                    "element.getElementsByTagName(\"button\")[0].click();"
-                        driver.execute_script(js_string)
-                        time.sleep(3)
+                # Satellite URL
+                if i == 1:
+                    url = 'https://www.google.com/maps/@{lat},{long},{z}z/data=!3m1!1e3' \
+                        .format(lat=latitude,
+                                long=longitude,
+                                z=zoom)
 
-                        js_string = "var element = document.getElementsByClassName(\"widget-settings-earth-item\")[0];" \
-                                    "element.getElementsByTagName(\"button\")[1].click();"
-                        driver.execute_script(js_string)
+                driver.get(url)
+                time.sleep(5)
 
-                    js_string = "var element = document.getElementById(\"omnibox-container\");element.remove();"
-                    driver.execute_script(js_string)
-                    js_string = "var element = document.getElementById(\"watermark\");element.remove();"
-                    driver.execute_script(js_string)
-                    js_string = "var element = document.getElementById(\"vasquette\");element.remove();"
-                    driver.execute_script(js_string)
-                    js_string = "var element = document.getElementsByClassName(\"app-viewcard-strip\");element[0].remove();"
-                    driver.execute_script(js_string)
+                # Remove labels from Satellite view
+                if i == 1:
+                    js_code_execute(driver, remove_labels[0])
+                    time.sleep(3)
+                    js_code_execute(driver, remove_labels[1])
 
-                    js_string = "var element = document.getElementsByClassName(\"scene-footer-container\");element[0].remove();"
-                    driver.execute_script(js_string)
+                for j in remove_from_view:
+                    js_code_execute(driver, j)
 
-                    # Let the map load all assets before taking a screenshot
-                    time.sleep(sleep_time)
-                    image = screenshot(screen_width, screen_height,
-                                       offset_left, offset_top,
-                                       offset_right, offset_bottom)
-                    # Scale image up or down if desired, then save in memory
-                    image = scale_image(image, scale)
-                    if i == 0:
-                        images_maps[row][col] = image
-                        # image.save(f"{outfile}-map-{row}-{col}.png")
-                        image.save(f"{outfile}-map-{c_map}.png")
-                        c_map += 1
-                    else:
-                        images_satellites[row][col] = image
-                        # image.save(f"{outfile}-{row}-{col}.png")
-                        image.save(f"{outfile}-{c_image}.png")
-                        c_image += 1
-            else:
-                c_map += 1
+                # Let the map load all assets before taking a screenshot
+                time.sleep(sleep_time)
+                image = screenshot(screen_width, screen_height, offset_left, offset_top, offset_right, offset_bottom)
 
-    driver.close()
-    driver.quit()
+                # Scale image up or down if desired, then save in memory
+                image = scale_image(image, scale)
+                if i == 0:
+                    # image.save(f"{outfile}-map-{row}-{col}.png")
+                    image.save(f"{outfile}-map-{c_map}.png")
+                    c_map += 1
+                else:
+                    # image.save(f"{outfile}-{row}-{col}.png")
+                    image.save(f"{outfile}-{c_image}.png")
+                    c_image += 1
+
+        driver.close()
+        driver.quit()
 
 
-# Combine all the images into one, then save it to disk
-# final_maps = combine_images(images_maps)
-#  final_satellite = combine_images(images_satellites)
-#   final_satellite.save(f"{outfile}.png")
-#   final_maps.save(f"{outfile}-map.png")
-
-
-# if not outfile:
-#     timestamp = datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
-#    outfile = 'testimg-{}.png'.format(timestamp)
+def js_code_execute(driver, js_string: str):
+    """Execute the JS code"""
+    driver.execute_script(js_string)
 
 
 def get_screen_resolution() -> tuple:
     """Return tuple of (width, height) of screen resolution in pixels."""
     root = tkinter.Tk()
     root.withdraw()
-    return (root.winfo_screenwidth(), root.winfo_screenheight())
+    return root.winfo_screenwidth(), root.winfo_screenheight()
 
 
-def calc_latitude_shift(screen_height: int, percent_hidden: float) -> float:
+def calc_latitude_shift(screen_height: int, percent_hidden: float, zoom: int) -> float:
     """Return the amount to shift latitude per row of screenshots."""
-    return -0.000002051 * screen_height * (1 - percent_hidden)
+    return -0.000002051 * screen_height * (1 - percent_hidden) * (1 / 1.7 ** (zoom - 18))
 
 
-def calc_longitude_shift(screen_width: int, percent_hidden: float) -> float:
+def calc_longitude_shift(screen_width: int, percent_hidden: float, zoom: int) -> float:
     """Return the amount to shift longitude per column of screenshots."""
-    return 0.00000268 * screen_width * (1 - percent_hidden)
+    return 0.00000268 * screen_width * (1 - percent_hidden) * (1 / 1.7 ** (zoom - 18))
 
 
 def screenshot(screen_width: int, screen_height: int,
@@ -178,23 +173,3 @@ def scale_image(image: Image, scale: float) -> Image:
     height = round(image.height * scale)
     image.thumbnail((width, height))
     return image
-
-
-def combine_images(images: list) -> Image:
-    """Return combined image from a grid of identically-sized images.
-
-    images is a 2d list of Image objects. The images should
-    be already sorted/arranged when provided to this function.
-    """
-    imgwidth = images[0][0].width
-    imgheight = images[0][0].height
-    newsize = (imgwidth * len(images[0]), imgheight * len(images))
-    newimage = Image.new('RGB', newsize)
-
-    # Add all the images from the grid to the new, blank image
-    for rowindex, row in enumerate(images):
-        for colindex, image in enumerate(row):
-            location = (colindex * imgwidth, rowindex * imgheight)
-            newimage.paste(image, location)
-
-    return newimage
